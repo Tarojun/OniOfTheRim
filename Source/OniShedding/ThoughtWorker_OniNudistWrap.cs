adabb3d8c4e73321aa7274d97d57735667894920
Tarojun <59377200+Tarojun@users.noreply.github.com>
2020-02-07 22:27:35 +0000

    Add files via upload

---
+++ Source/OniShedding/ThoughtWorker_OniNudistWrap.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace OniShedding
+{
+    public class ThoughtWorker_OniNudistWrap : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            List<Apparel> wornApparel = p.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (p.def.defName == "AOni")
+                {
+                    if (p.apparel.WornApparel.Any(x => x.def.defName == "Apparel_OniWrapNudist"))
+                    { 
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
